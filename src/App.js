8f1c3277ac6eb582878feb274f76bdf9b9ca4a45
Md Emamudin <mdemamudin726@gmail.com>
2024-08-11 20:21:49 +0530

    fixed bug

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
       />
         <Routes>
           {/* since same news component is render everytime so we need to give unique key to everyone */}
-          <Route exact path="/home" element={<Newss setProgress={setProgress} apikey={apikey} key="home" pageSize={9} country={"in"} category={"general"}/>}/>
+          <Route exact path="/" element={<Newss setProgress={setProgress} apikey={apikey} key="home" pageSize={9} country={"in"} category={"general"}/>}/>
           <Route exact path="/business" element={<Newss setProgress={setProgress} apikey={apikey} key="business"  pageSize={9} country={"in"} category={"business"}/>}/>
           <Route exact path="/entertainment" element={<Newss setProgress={setProgress} apikey={apikey} key="entertainment" pageSize={9} country={"in"} category={"entertainment"}/>}/>
           <Route exact path="/general" element={<Newss setProgress={setProgress} apikey={apikey} key="general" pageSize={9} country={"in"} category={"general"}/>}/>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className=" navbar fixed-top navbar-expand-md navbar-dark bg-dark">
-        <NavLink className="navbar-brand" to="#">NewsBhandar</NavLink>
+        <p className="navbar-brand mb-1" >NewsBhandar</p>
         <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ">
             <li className="nav-item ">
-              <NavLink className="nav-link" to="/home">Home <span className="sr-only">(current)</span></NavLink>
+              <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
             </li>
             <li className="nav-item">
               <NavLink className="nav-link" to="/business">Business</NavLink>
